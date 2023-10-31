62b2b9f43ff6d235a9eaf8541d2a6d556e9ca36f
kilianAid <kilian_bs@mail.ru>
2023-10-31 16:56:04 +0500

    Task1

---
+++ Tyuiu.KornilovKA.Sprint4.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KornilovKA.Sprint4.Task1.V21.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint4.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] arrayNums = { 2, 4, 8, 5, 2, 6, 5, 8, 8, 6, 6 };
+
+            var res = ds.Calculate(arrayNums);
+            int wait = 1769472;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
