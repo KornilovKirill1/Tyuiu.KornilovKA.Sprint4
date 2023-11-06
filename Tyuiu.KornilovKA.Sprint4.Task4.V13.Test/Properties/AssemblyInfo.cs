2c4749a261442914edb2b7aeba70460159a9be1e
kilianAid <kilian_bs@mail.ru>
2023-11-06 23:31:07 +0500

    Task4

---
+++ Tyuiu.KornilovKA.Sprint4.Task4.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KornilovKA.Sprint4.Task4.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KornilovKA.Sprint4.Task4.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("949c5a7c-1c04-4282-9ead-c9555c120382")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
