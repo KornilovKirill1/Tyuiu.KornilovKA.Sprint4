1b327b14036b2c07311e551b1dfe03990eb2918b
kilianAid <kilian_bs@mail.ru>
2023-11-07 09:22:19 +0500

    Task5

---
+++ Tyuiu.KornilovKA.Sprint4.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KornilovKA.Sprint4.Task5.V30.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint4.Task5.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] arrayNums = new int[5, 5] { { 2, -1, 1, 2, 2 },
+                                               { 0, 2, 3, -2, 4 },
+                                               { 2, 2, 2, -1, 0 },
+                                               { -2, 2, 4, 3, 2 },
+                                               { 1, -1, 2, -1, 4 } };
+            int res = ds.Calculate(arrayNums);
+            int wait = 40;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KornilovKA.Sprint4.Task5.V30/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KornilovKA.Sprint4.Task5.V30.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint4.Task5.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил: Корнилов К. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Корнилов Кирилл Андреевич | ИИПб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -2 до 4. Найти сумму положительных *");
+            Console.WriteLine("* элементов.                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] arrayNums = new int[5, 5];
+
+            for (int i = 0; i < arrayNums.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayNums.GetLength(1); j++)
+                {
+                    arrayNums[i, j] = rnd.Next(-2, 5);
+                }
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arrayNums.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayNums.GetLength(1); j++)
+                {
+                    Console.Write($"{arrayNums[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма положительных элементов = " + ds.Calculate(arrayNums));
+
+            Console.ReadLine();
+        }
+    }
+}
