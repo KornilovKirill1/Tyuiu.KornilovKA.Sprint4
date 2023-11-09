e5a68ad2ce11c608dc44530e166824b113f148ed
kilianAid <kilian_bs@mail.ru>
2023-11-09 10:04:58 +0500

    Task7

---
+++ Tyuiu.KornilovKA.Sprint4.Task7.V3.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KornilovKA.Sprint4.Task7.V3.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint4.Task7.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int rows = 4;
+            int columns = 2;
+
+            string str = "27182818";
+
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 3;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KornilovKA.Sprint4.Task7.V3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KornilovKA.Sprint4.Task7.V3.Lib;
+
+namespace Tyuiu.KornilovKA.Sprint4.Task7.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Корнилов К. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Корнилов Кирилл Андреевич | ИИПб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"27182818\". Преобразуйте ее в матрицу *");
+            Console.WriteLine("* 4 на 2 и подсчитайте количество нечетных чисел в матрице.               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 4;
+            int columns = 2;
+            int[,] matrix = new int[rows, columns];
+
+            string str = "27182818";
+
+            int index = 0;
+            
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Количество нечетных чисел = " + ds.Calculate(rows, columns, str));
+
+            Console.ReadLine();
+        }
+    }
+}
